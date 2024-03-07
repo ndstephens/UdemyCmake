d68aa5a19fd5cada9e5a004f039843de72c9459a
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-03-07 15:19:18 -0500

    external libraries: git submodules

--- 2_CMake/00_MyProject/app/main.cc
+++ 2_CMake/00_MyProject/app/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
 #include "config.hpp"
 #include "my_lib.h"
 
@@ -7,6 +9,9 @@ int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
+
+    std::cout << "JSON Lib Version: " << NLOHMANN_JSON_VERSION_MAJOR << '.' << NLOHMANN_JSON_VERSION_MINOR << '.'
+              << NLOHMANN_JSON_VERSION_PATCH << '\n';
 
     print_hello_world();
 
